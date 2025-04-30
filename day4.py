03072e6a40aac8aecd12b318833f7c1397a5ce17
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-04-30 20:46:26 +0200

    hour 2

--- day4.py
+++ day4.py
@@ -83,4 +83,109 @@ get_discounts([2, 4, 6, 11], "50%") # [1, 2, 3, 5.5]
 
 get_discounts([10, 20, 40, 80], "75%") # [7.5, 15, 30, 60]
 
-get_discounts([100], "45%") # [45]
+get_discounts([100], "45%") # [45}
+
+
+
+
+def scale_tip(lst):
+        res = lst.index("I")
+        p = lst[:res]
+        s = lst[res+1:]
+        if sum(p) > sum(s):
+                return "left"
+        elif sum(p) == sum(s):
+                return "balanced"
+        else:
+                return "right"
+       
+
+
+
+
+print(scale_tip([0, 0, "I", 1, 1])) # "right"
+# 0 < 2 so it will tip right
+
+scale_tip([1, 2, 3, "I", 4, 0, 0]) # "left"
+# 6 > 4 so it will tip left
+
+scale_tip([5, 5, 5, 0, "I", 10, 2, 2, 1]) # "balanced"
+# # 15 = 15 so it will stay balanced
+
+def num_to_dashes(num):
+        return "-" * num
+
+
+
+num_to_dashes(1) # "-"
+
+num_to_dashes(5) # "-----"
+
+num_to_dashes(3) # "---"
+
+def word(s):
+    d = {
+        "one"	:1,
+        "two"	:2,
+        "three"	:3,
+        "four"	:4,
+        "five"	:5,
+        "six"	:6,
+        "seven"	:7,
+        "eight"	:8,
+        "nine"	:9,
+        "zero"	:0
+        }
+    return d.get(s)
+
+
+print(word("one")) # 1
+
+word("two") # 2
+
+word("nine") # 9
+
+
+def char_count(txt1, txt2):
+        return txt2.count(txt1)
+char_count("a", "edabit") # 1
+
+print(char_count("c", "Chamber of secrets")) # 1
+
+print(char_count("b", "big fat bubble")) # 4
+
+
+
+def divisible(num):
+        return True if num % 100 ==0 else False
+
+divisible(1) # False
+
+print(divisible(1000)) # True
+
+divisible(100) # True
+
+def profitable_gamble(prob, prize, pay):
+        return prob * prize > pay
+
+
+print(profitable_gamble(0.2, 50, 9)) # True
+
+profitable_gamble(0.9, 1, 2) # False
+
+profitable_gamble(0.9, 3, 2) # True
+
+
+def comp(txt1, txt2):
+        return len(txt1) == len(txt2)
+
+comp("AB", "CD") # True
+
+comp("ABC", "DE") # False
+
+print(comp("hello", "edabit")) # False
+
+print(len("dbi"))
+
+def is_even(n):
+	return n % 2 == 0
